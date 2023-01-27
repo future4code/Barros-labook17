76f926a7dac38ead57780799a342c3c57fe33693
gabriel-suela <gscsuela@gmail.com>
2023-01-27 19:10:06 -0300

    project model

---
+++ src/model/PostDTO.ts
@@ -0,0 +1,14 @@
+export interface PostInputDTO {
+    photo: string,
+    description: string,
+    type: string, 
+    authorId: string
+}
+
+export interface PostCreateDTO {
+    id: string,
+    photo: string,
+    description: string,
+    type: string, 
+    authorId: string
+}
